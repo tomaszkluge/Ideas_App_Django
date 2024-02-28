0b3fe75f30338bd4601589a49d209d586c7f54a2
tomaszkluge <tomasz.kluge@gmail.com>
2024-02-28 16:17:55 +0100

    Made database and admin panel in Django

--- klugcode/ideas/admin.py
+++ klugcode/ideas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Idea, Vote
 
-# Register your models here.
+
+admin.site.register(Idea)
+admin.site.register(Vote)
--- klugcode/ideas/models.py
+++ klugcode/ideas/models.py
@@ -11,8 +11,8 @@ IDEA_STATUS = (
 class Idea(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    websites = models.URLField()
-    status = models.CharField(choices=IDEA_STATUS, default='pending')
+    websites = models.URLField(null=True, blank=True)
+    status = models.CharField(choices=IDEA_STATUS, max_length=30, default='pending')
 
 
 class Vote(models.Model):
