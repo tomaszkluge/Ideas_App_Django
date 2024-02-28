cd2139e6ffac36d64afd6ecadcbead7cdfa5bc49
tomaszkluge <tomasz.kluge@gmail.com>
2024-02-28 11:04:30 +0100

    Added Vote class

--- klugcode/ideas/models.py
+++ klugcode/ideas/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Idea(models.Model):
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length=255)
     description = models.TextField()
 
+
+class Vote(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    reason = models.TextField()
