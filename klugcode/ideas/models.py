debd00297201d689bfcef26397437456731f6384
tomaszkluge <tomasz.kluge@gmail.com>
2024-02-28 12:23:37 +0100

    some changes in class Idea and class Vote

--- klugcode/ideas/models.py
+++ klugcode/ideas/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+IDEA_STATUS = (
+    ('pending', 'Waiting for review'),
+    ('accepted', 'Accepted'),
+    ('done', 'Done'),
+    ('rejected', 'Rejected')
+)
+
 
 class Idea(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    websites = models.URLField()
+    status = models.CharField(choices=IDEA_STATUS, default='pending')
 
 
 class Vote(models.Model):
