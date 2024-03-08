9783eae028d54ca6aa9d6e746435ecca30f819ac
tomaszkluge <tomasz.kluge@gmail.com>
2024-03-08 13:36:39 +0100

    added class CommentSerializer

--- klugcode/ideas/serializer.py
+++ klugcode/ideas/serializer.py
@@ -2,7 +2,13 @@ from .models import Idea, Comment
 from rest_framework import serializers
 
 
-class IdeaSerializer(serializers.HyperLinkedModels):
+class IdeaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Idea
+        fields = ['id', 'title', 'description', 'websites', 'status']
 
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['idea', 'reason']
