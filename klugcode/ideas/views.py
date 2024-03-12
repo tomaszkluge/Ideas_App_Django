145a7148540a242d10aee17d5763fd9f2cd56ce7
tomaszkluge <tomasz.kluge@gmail.com>
2024-03-12 23:41:21 +0100

    start building views

--- klugcode/ideas/views.py
+++ klugcode/ideas/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Idea, Comment
+from rest_framework import viewsets
+from .serializer import IdeaSerializer, CommentSerializer
 
-# Create your views here.
+class IdeaViewSet(viewsets.ModelViewSet):
+    queryset = Idea.objects.all()
+    serializer_class = IdeaSerializer
+
